b82b551d96874144062d4c07113d08a0cb3d18f3
Je06jm <jeremymoyes3@gmail.com>
2024-04-28 20:52:46 -0600

    Removed namespace from sockets

--- app/Socket.hpp
+++ app/Socket.hpp
@@ -7,53 +7,49 @@
 #include <string>
 #include <mutex>
 
-namespace platform {
+class TCPSocket {
+protected:
+    mutable std::mutex lock;
 
-    class TCPSocket {
-    protected:
-        mutable std::mutex lock;
+public:
+    virtual ~TCPSocket() = default;
 
-    public:
-        virtual ~TCPSocket() = default;
+    virtual bool IsOpen() const = 0;
+    virtual bool IsServer() const = 0;
 
-        virtual bool IsOpen() const = 0;
-        virtual bool IsServer() const = 0;
+    virtual void Send(const void* data, size_t size) const = 0;
+    virtual size_t Recv(void* buffer, size_t max_size) const = 0;
 
-        virtual void Send(const void* data, size_t size) const = 0;
-        virtual size_t Recv(void* buffer, size_t max_size) const = 0;
+    virtual std::shared_ptr<TCPSocket> Accept() const = 0;
 
-        virtual std::shared_ptr<TCPSocket> Accept() const = 0;
+    static std::shared_ptr<TCPSocket> CreateServer(uint16_t port);
+    static std::shared_ptr<TCPSocket> CreateClient(const std::string& address, uint16_t port);
+};
 
-        static std::shared_ptr<TCPSocket> CreateServer(uint16_t port);
-        static std::shared_ptr<TCPSocket> CreateClient(const std::string& address, uint16_t port);
-    };
+class UDPSocket {
+protected:
+    mutable std::mutex lock;
 
-    class UDPSocket {
-    protected:
-        mutable std::mutex lock;
+public:
+    using ClientID = uint32_t;
 
-    public:
-        using ClientID = uint32_t;
+protected:
+    static ClientID next_free_id;
+
+    const uint16_t port;
+
+public:
+    UDPSocket(uint16_t port) : port{port} {}
     
-    protected:
-        static ClientID next_free_id;
+    virtual ~UDPSocket() = default;
 
-        const uint16_t port;
-    
-    public:
-        UDPSocket(uint16_t port) : port{port} {}
-        
-        virtual ~UDPSocket() = default;
+    virtual bool IsOpen() const = 0;
 
-        virtual bool IsOpen() const = 0;
+    virtual void Send(const void* data, size_t size, ClientID to = 0) const = 0;
+    virtual size_t Recv(void* buffer, size_t max_size, ClientID& from) const = 0;
 
-        virtual void Send(const void* data, size_t size, ClientID to = 0) const = 0;
-        virtual size_t Recv(void* buffer, size_t max_size, ClientID& from) const = 0;
-
-        static std::shared_ptr<UDPSocket> CreateServer(uint16_t port);
-        static std::shared_ptr<UDPSocket> CreateClient(const std::string& address, uint16_t port);
-    };
-
-}
+    static std::shared_ptr<UDPSocket> CreateServer(uint16_t port);
+    static std::shared_ptr<UDPSocket> CreateClient(const std::string& address, uint16_t port);
+};
 
 #endif
