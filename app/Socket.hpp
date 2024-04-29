f0cca3ee2ffcf11c11446d06b6216177e1480ffe
Je06jm <jeremymoyes3@gmail.com>
2024-04-28 20:54:31 -0600

    Removed UDPSocket

--- app/Socket.hpp
+++ app/Socket.hpp
@@ -26,30 +26,4 @@ public:
     static std::shared_ptr<TCPSocket> CreateClient(const std::string& address, uint16_t port);
 };
 
-class UDPSocket {
-protected:
-    mutable std::mutex lock;
-
-public:
-    using ClientID = uint32_t;
-
-protected:
-    static ClientID next_free_id;
-
-    const uint16_t port;
-
-public:
-    UDPSocket(uint16_t port) : port{port} {}
-    
-    virtual ~UDPSocket() = default;
-
-    virtual bool IsOpen() const = 0;
-
-    virtual void Send(const void* data, size_t size, ClientID to = 0) const = 0;
-    virtual size_t Recv(void* buffer, size_t max_size, ClientID& from) const = 0;
-
-    static std::shared_ptr<UDPSocket> CreateServer(uint16_t port);
-    static std::shared_ptr<UDPSocket> CreateClient(const std::string& address, uint16_t port);
-};
-
 #endif
