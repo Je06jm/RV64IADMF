21a489a6654c03b57d9efadce686db4c32cbd227
Je06jm <jeremymoyes3@gmail.com>
2024-05-06 09:02:24 -0600

    Updated bios to test virtual memory

--- bios/main.c
+++ bios/main.c
@@ -1,14 +1,16 @@
 #include "printf.h"
 #include "input.h"
 #include "machine.h"
-#include "trap.h"
+#include "virt_mem.h"
 
 int main(int argc, const char** argv) {
     //phys_setup();
 
-    trigger_mtrap(0x12, TRAP_PRIVILEGE_USER);
+    virt_mem_setup();
 
     printf("Hello!\n");
+
+    printf("This is the Supervisor!\n");
 
     while (1) {}
     
---
+++ bios/virt_mem.h
@@ -0,0 +1,6 @@
+#ifndef VIRT_MEM_H
+#define VIRT_MEM_H
+
+void virt_mem_setup();
+
+#endif
