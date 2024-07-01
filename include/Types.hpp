74d0ecd06aa56a29eaa57b139ef50572e07668a2
Je06jm <jeremymoyes3@gmail.com>
2024-06-30 18:37:17 -0600

    Added ranges to types

--- include/Types.hpp
+++ include/Types.hpp
@@ -19,4 +19,31 @@ using SByte = int8_t;
 
 using Hart = uint64_t;
 
+#undef LONG_MIN
+#undef LONG_MAX
+
+constexpr Long LONG_MIN = 0;
+constexpr Long LONG_MAX = UINT64_MAX;
+
+constexpr Word WORD_MIN = 0;
+constexpr Word WORD_MAX = UINT32_MAX;
+
+constexpr Half HALF_MIN = 0;
+constexpr Half HALF_MAX = UINT16_MAX;
+
+constexpr Byte BYTE_MIN = 0;
+constexpr Byte BYTE_MAX = UINT8_MAX;
+
+constexpr SLong SLONG_MIN = INT64_MIN;
+constexpr SLong SLONG_MAX = INT64_MAX;
+
+constexpr SWord SWORD_MIN = INT32_MIN;
+constexpr SWord SWORD_MAX = INT32_MAX;
+
+constexpr SHalf SHALF_MIN = INT16_MIN;
+constexpr SHalf SHALF_MAX = INT16_MAX;
+
+constexpr SByte SBYTE_MIN = INT8_MIN;
+constexpr SByte SBYTE_MAX = INT8_MAX;
+
 #endif
