705d0962a784d269e73ddaf6497db63335a1a345
Je06jm <jeremymoyes3@gmail.com>
2024-07-29 22:51:57 -0500

    Test cas LUI now uses random numbers

--- test/LUI.cpp
+++ test/LUI.cpp
@@ -6,15 +6,25 @@ DEFINE_TESTCASE(LUI, "LUI") {
 
     ADD_RAM(0x1000, 0x1000);
 
+    auto value = Random<uint64_t>(0, UINT32_MAX);
+
     memory.WriteWord(0x1000, RV64_U(
         RVInstruction::OP_LUI,
         VirtualMachine::REG_A0,
-        0x1
+        static_cast<uint32_t>(value)
     ));
+
+    value <<= 12;
+
+    value &= 0xffffffff;
+
+    if (value & 0x80000000LL)
+        value |= 0xffffffff00000000LL;
 
     STEP_VMS(1);
 
-    ASSERT(vm.GetRegister(VirtualMachine::REG_A0).Value().u32 == 0x1000, "Produces the wrong result");
+    auto& reg = vm.GetRegister(VirtualMachine::REG_A0).Value();
+    ASSERT(reg.u64 == value, "Produces the wrong result. Expecting {:x}, got {:x}", value, reg.u64);
 
     SUCCESS;
 }
