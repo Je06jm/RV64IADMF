e57c2788139e3c56948ca8f84d4fd5b242c96808
Jeremy <jeremymoyes3@gmail.com>
2024-09-27 15:04:15 -0500

    Added REMUW test case

---
+++ test/REMUW.cpp
@@ -0,0 +1,41 @@
+#include "Test.hpp"
+
+DEFINE_TESTCASE(REMUW) {
+    SETUP_MEMORY;
+    SETUP_VM(0x1000);
+
+    ADD_RAM(0x1000, 0x1000);
+
+    auto lhs = Random<Word>(0, WORD_MAX);
+    auto rhs = Random<Word>(0, WORD_MAX);
+
+    auto sel_rs1 = Random<size_t>(1, VirtualMachine::REGISTER_COUNT);
+    auto sel_rs2 = Random<size_t>(1, VirtualMachine::REGISTER_COUNT);
+    auto sel_rd = Random<size_t>(1, VirtualMachine::REGISTER_COUNT);
+
+    auto& rs1 = vm.GetRegister(sel_rs1).Value();
+    auto& rs2 = vm.GetRegister(sel_rs2).Value();
+    auto& rd = vm.GetRegister(sel_rd).Value();
+
+    rs1.u64 = 0;
+    rs2.u64 = 0;
+    rs1.u32 = lhs;
+    rs2.u32 = rhs;
+
+    auto expected = rs1.u64 % rs2.u64;
+
+    memory.WriteWord(0x1000, RV64_R(
+        RVInstruction::OP_MATH_W,
+        sel_rd,
+        RVInstruction::FUNCT3_AND_REMU,
+        sel_rs1,
+        sel_rs2,
+        RVInstruction::FUNCT7_REMU
+    ));
+
+    STEP_VMS(1);
+
+    ASSERT(rd.u64 == expected, " Wrong remuw result. Expected {:x}, got {:x}", expected, rd.u64);
+
+    SUCCESS;
+}
